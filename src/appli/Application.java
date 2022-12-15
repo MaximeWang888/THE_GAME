56b17e751490c56fbe167b39712727146699f5c9
Maxime Wang <888maximewang@gmail.com>
2022-12-15 20:22:02 +0100

    formatting code

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -8,7 +8,8 @@ import java.util.*;
 
 /**
  * Modélise l'application lancant une partie entre deux joueurs.
- * @author  Maxime Wang, Sanaa Abdellah, Minh-Tri NGUYEN
+ *
+ * @author Maxime Wang, Sanaa Abdellah, Minh-Tri NGUYEN
  * @version 1.1
  */
 public class Application {
@@ -31,15 +32,17 @@ public class Application {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_VERT = "\u001B[32m";
     public static ArrayList<ICarte> oldCartes = new ArrayList<>();
+
     private static void menu() {
         String choix;
         System.out.println(ANSI_YELLOW + "Bienvenue dans “The game - le duel” \n" + ANSI_RESET);
         System.out.println(ANSI_BLUE + "Pour une meilleure expérience de jeu," +
-                " veuillez agrandir votre fenêtre de la console.\n(ou bien l'épingler sur le côté droit de votre IDE) \n" + ANSI_RESET);
+                " veuillez agrandir votre fenêtre de la console.\n(ou bien l'épingler sur le" +
+                " côté droit de votre IDE) \n" + ANSI_RESET);
         boolean estTypeAttendu = false;
 
         while (!estTypeAttendu) {
-            System.out.println("\033[0;31m" + "MENU " + ANSI_RESET + "(faites votre choix) " );
+            System.out.println("\033[0;31m" + "MENU " + ANSI_RESET + "(faites votre choix) ");
             System.out.println("1 : règles du jeu \n");
             System.out.println("2 : lancer une partie \n");
             System.out.println("3 : quitter \n");
@@ -48,37 +51,45 @@ public class Application {
             choix = sc.nextLine();
 
             switch (choix) {
-                case "1":
-                {
+                case "1": {
                     System.out.println(ANSI_VERT + "\n---------------------------" +
                             "----------------------------------------------------" +
                             "----------------------------------------------------" +
                             "--------------------------------------" + ANSI_RESET);
-                    System.out.println("Règles du jeu : \nLe jeu “The game - le duel” est un jeu de cartes qui se joue " +
-                            "entre deux joueurs, chacun a 60 cartes et une pile ascendante et descendante. \n ");
-                    System.out.println("Le but du jeu est de vider ses cartes mais vous gagnez aussi si votre " +
-                            "adversaire ne peut plus jouer.\n");
-                    System.out.println("Chaque joueur commence avec six cartes en main et récupère entre deux et " +
-                            "six cartes selon le coup qu’il a joué.");
-                    System.out.println("Il y a plusieurs règles de pose mais la principale et que sur le tas ascendant " +
-                            "vous pouvez mettre que des cartes strictement croissantes et inversement sur l’autre tas.\n");
-                    System.out.println("On doit jouer au minimum deux cartes minimum et au maximum six cartes\n" +
-                            "Pour jouer la carte n°2 sur la pile montante : 02^\n"+
-                            "Pour jouer la carte n°2 sur la pile descendante : 02v\n");
-                    System.out.println("^[01] représente le tas de cartes montante \nv[60] représente le tas de cartes descendante\n");
-                    System.out.println(ANSI_RED+"Cas spécial"+ANSI_RESET+" : Le joueur peut poser sa carte dans la adverse si et seulement si la carte du joueur" +
-                            " est : \n- une dizaine au dessus si c'est une pile montante\n- une dizaine en dessous si c'est une pile descendante");
-                    System.out.println("Exemple : Si sur la pile montante de l'adversaire il y a la carte 08 posée et que le joueur possède la carte 18, " +
+                    System.out.println("Règles du jeu : \nLe jeu “The game - le duel” est un" +
+                            " jeu de cartes qui se joue entre deux joueurs, chacun a 60 cartes " +
+                            "et une pile ascendante et descendante. \n ");
+                    System.out.println(
+                            "Le but du jeu est de vider ses cartes mais vous gagnez aussi si votre " +
+                                    "adversaire ne peut plus jouer.\n");
+                    System.out.println(
+                            "Chaque joueur commence avec six cartes en main et récupère entre deux et " +
+                                    "six cartes selon le coup qu’il a joué.");
+                    System.out.println("Il y a plusieurs règles de pose mais la principale et " +
+                            "que sur le tas ascendant vous pouvez mettre que des cartes strictement " +
+                            "croissantes et inversement sur l’autre tas.\n");
+                    System.out.println(
+                            "On doit jouer au minimum deux cartes minimum et au maximum six cartes\n" +
+                                    "Pour jouer la carte n°2 sur la pile montante : 02^\n" +
+                                    "Pour jouer la carte n°2 sur la pile descendante : 02v\n");
+                    System.out.println("^[01] représente le tas de cartes montante \nv[60] " +
+                            "représente le tas de cartes descendante\n");
+                    System.out.println(ANSI_RED + "Cas spécial" + ANSI_RESET + " : Le joueur peut " +
+                            "poser sa carte dans la adverse si et seulement si la carte du joueur" +
+                            " est : \n- une dizaine au dessus si c'est une pile montante\n- " +
+                            "une dizaine en dessous si c'est une pile descendante");
+                    System.out.println("Exemple : Si sur la pile montante de l'adversaire il y a " +
+                            "la carte 08 posée et que le joueur possède la carte 18, " +
                             "\nle joueur peut alors poser sa carte sur la pile montante de l'adversaire");
-                    System.out.println("Pour poser la carte n°18 sur la pile montante de l'adversaire, il faut ajouter une apostrophe : 18^' ");
+                    System.out.println("Pour poser la carte n°18 sur la pile montante de l'adversaire, il" +
+                            " faut ajouter une apostrophe : 18^' ");
                     System.out.println(ANSI_VERT + "---------------------------" +
                             "----------------------------------------------------" +
                             "----------------------------------------------------" +
                             "--------------------------------------\n" + ANSI_RESET);
                     break;
                 }
-                case "2":
-                {
+                case "2": {
                     String nomJoueur1, nomJoueur2;
                     System.out.println(ANSI_VERT + "\n---------------------------" +
                             "----------------------------------------------------" +
@@ -99,7 +110,8 @@ public class Application {
                     IJoueur jS = new Joueur(nomJoueur2);
 
                     // Tant que le jeu n'est pas terminé
-                    while ((jN.aDesCartes() && jS.aDesCartes()) && (jN.peutPoserDesCartes(jS) && jS.peutPoserDesCartes(jN))) {
+                    while ((jN.aDesCartes() && jS.aDesCartes()) &&
+                            (jN.peutPoserDesCartes(jS) && jS.peutPoserDesCartes(jN))) {
                         oldCartes = getOldCartes(jN, jS);
                         afficherLesPilesDesJoueurs(jN, jS, oldCartes);
                         if (jN.aMonTourDeJouer(jS)) {
@@ -129,10 +141,11 @@ public class Application {
 
     /**
      * Joue un coup
-     * @param j Le joueur qui joue son coup
+     *
+     * @param j           Le joueur qui joue son coup
      * @param jAdversaire Le joueur adverse qui ne joue pas
      */
-    private static void joue (IJoueur j, IJoueur jAdversaire){
+    private static void joue(IJoueur j, IJoueur jAdversaire) {
         // Dans le cas général, un affichage plus une demande de saisie au joueur
         System.out.print("\nFaites votre jeu => ");
         String cartesJouer = sc.nextLine();
@@ -153,6 +166,7 @@ public class Application {
         System.out.println(j.getCoup().getCoups().size() + " cartes posées, " +
                 nbDeCarteAPiocher + " cartes piochées ");
     }
+
     private static ArrayList<ICarte> getOldCartes(IJoueur jN, IJoueur jS) {
         ArrayList listOldCartes = new ArrayList<ICarte>();
         listOldCartes.add(jN.getAscendant());
@@ -160,14 +174,16 @@ public class Application {
         listOldCartes.add(jS.getAscendant());
         listOldCartes.add(jS.getDescendant());
 
-        return listOldCartes ;
+        return listOldCartes;
     }
+
     /**
      * Permet d'avoir une réprésentation textuelle des piles des joueurs
+     *
      * @param jN le joueur NORD
      * @param jS le joueur SUD
      */
-    private static void afficherLesPilesDesJoueurs (IJoueur jN, IJoueur jS,List<ICarte> oldCartes){
+    private static void afficherLesPilesDesJoueurs(IJoueur jN, IJoueur jS, List<ICarte> oldCartes) {
         Formatter fmt = new Formatter();
         Formatter fm = new Formatter();
         int nbSpace = jN.getNom().length() + jS.getNom().length();
@@ -181,7 +197,7 @@ public class Application {
         for (int i = 0; i < nbSpace; i++) {
             System.out.print(" ");
         }
-        System.out.println("(" + jN.getNbDeMesCartes()+
+        System.out.println("(" + jN.getNbDeMesCartes() +
                 " cartes en main ET " + jN.getNbDeMaPioche() + " cartes dans ma pioche)");
 
         // Affichage des piles de cartes (joueur 2)
@@ -199,11 +215,12 @@ public class Application {
     /**
      * Permet d'avoir une représentation du joueur gagnant
      * sous la forme d'une chaîne de caractères.
+     *
      * @param jN le joueur NORD
      * @param jS le joueur SUD
      * @return la représentation textuelle du gagnant de la partie
      */
-    private static StringBuilder leGagnant (IJoueur jN, IJoueur jS){
+    private static StringBuilder leGagnant(IJoueur jN, IJoueur jS) {
         // Construit un StringBuilder pour l'affichage du gagnant de la partie
         StringBuilder phraseDuGagnant = new StringBuilder();
 
@@ -225,5 +242,3 @@ public class Application {
         return phraseDuGagnant;
     }
 }
-
-
